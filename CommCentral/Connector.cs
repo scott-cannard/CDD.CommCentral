d103159f52258e38aacb6ff7373274c561cb93c5
scott-cannard <scott.cannard@hotmail.com>
2016-02-20 16:29:50 -0800

    Add initial project files to new repo

---
+++ CommCentral/Connector.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace CommCentral.Connection
+{
+    public enum ConnectorType { QueueHolder, Publisher, Subscriber };
+
+    public class Connector
+    {
+        private ConnectorType m_TypeEnum;
+        private string m_TypeStr;
+        private TcpListener m_Listener;
+        private IPEndPoint m_EndPoint;
+        private Thread m_Worker;
+
+        public Connector(ConnectorType connectorType, System.Net.IPEndPoint endPoint, bool startThreadImmediately = true)
+        {
+            m_TypeEnum = connectorType;
+            m_TypeStr = Enum.GetName(typeof(ConnectorType), connectorType);
+            m_EndPoint = endPoint;
+
+            m_Worker = new Thread(this.Run);
+
+            if (startThreadImmediately)
+                m_Worker.Start();
+        }
+
+        private void Run()
+        {
+            m_Listener = new TcpListener(m_EndPoint);
+            m_Listener.Start();
+            Program.Print(m_TypeStr + ": Listener started\n");
+
+            while (m_StayAlive)
+            {
+                Program.Print(m_TypeStr + ": Waiting for connection request on port " + m_EndPoint.Port + "\n");
+                try
+                {
+                    m_Listener.AcceptSocket();
+                }
+                catch (SocketException ex)
+                {
+                    Program.Print(m_TypeStr + ": " + ex.Message + '\n');
+                    continue;
+                }
+                Program.Print(m_TypeStr + ": Received connection request\n");
+                Program.Print(m_TypeStr + ": TODO - Process incoming connection request\n");
+            }
+            //Clean up
+            m_Listener.Stop();
+            Program.Print(m_TypeStr + ": Listener stopped\n");
+        }
+
+        private volatile bool m_StayAlive = true;
+        public void RequestStop()
+        {
+            m_StayAlive = false;
+            m_Listener.Stop();
+        }
+        public void Join()
+        {
+            m_Worker.Join();
+            Program.Print(m_TypeStr + ": Worker has terminated\n");
+        }
+        public bool Join(int msTimeout)
+        {
+            if (m_Worker.Join(msTimeout))
+            {
+                Program.Print(m_TypeStr + ": Worker has terminated\n");
+                return true;
+            }
+            //else
+            Program.Print(m_TypeStr + ": Thread join timed out, worker has gone rogue!");
+            return false;
+        }
+    }
+}
---
+++ CommCentral/Program.cs
@@ -0,0 +1,45 @@
+﻿using CommCentral.Connection;
+using System;
+using System.Net;
+
+namespace CommCentral
+{
+    class Program
+    {
+        const int PORT_QUEUEHOLDER = 790;
+        const int PORT_PUBLISHER   = 791;
+        const int PORT_SUBSCRIBER  = 792;
+
+        static void Main(string[] args)
+        {
+            Program.Print("Main: Beginning execution\n");
+
+            //Listen for QueueHolders
+            Connector qholderHandler = new Connector(ConnectorType.QueueHolder, new IPEndPoint(IPAddress.Parse("127.0.0.1"), PORT_QUEUEHOLDER));
+            //Listen for Publishers
+            Connector publisherHandler = new Connector(ConnectorType.Publisher, new IPEndPoint(IPAddress.Parse("127.0.0.1"), PORT_PUBLISHER));
+            //Listen for Subscribers
+            Connector subscriberHandler = new Connector(ConnectorType.Subscriber, new IPEndPoint(IPAddress.Parse("127.0.0.1"), PORT_SUBSCRIBER));
+
+            while (!Console.KeyAvailable) ;
+            while (Console.KeyAvailable)
+                Console.ReadKey(true);
+
+            publisherHandler.RequestStop();
+            subscriberHandler.RequestStop();
+            publisherHandler.Join();
+            subscriberHandler.Join();
+
+            qholderHandler.RequestStop();
+            qholderHandler.Join();
+
+            Program.Print("Main: Terminating\n\n**Press <ENTER> to quit**");
+            Console.In.ReadLine();
+        }
+
+        public static void Print(string content)
+        {
+            Console.Out.Write(content);
+        }
+    }
+}
